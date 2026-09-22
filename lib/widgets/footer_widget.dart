aa0ea7a91149f609d49f5cee432ea9a7a51dca36
Janos Raul Szabo <janos.szabo@gmail.com>
2026-09-22 19:43:11 +0300

    update: footer

--- lib/widgets/footer_widget.dart
+++ lib/widgets/footer_widget.dart
@@ -181,7 +181,7 @@ class FooterWidget extends StatelessWidget {
       icon: FontAwesomeIcons.users,
       links: [
         _FooterLink('Discord', 'https://discord.gg/wCmdJGXFr7', icon: FontAwesomeIcons.discord),
-        _FooterLink('Telegram', 'https://t.me/official_bitcoinsilver', icon: FontAwesomeIcons.telegram),
+        _FooterLink('Telegram', 'https://t.me/official_bitcoin_silver', icon: FontAwesomeIcons.telegram),
         _FooterLink('Twitter / X', 'https://x.com/Official_BTCS', icon: FontAwesomeIcons.xTwitter),
       ],
     );
